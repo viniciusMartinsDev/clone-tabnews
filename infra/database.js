fa97e50dd2e5d4449aad463d9c1b77d428fea206
Vinícius Martins <mpviniciusmp@gmail.com>
2024-09-24 10:52:34 -0300

    makes database.js client.connect() more robust to erros

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,13 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
